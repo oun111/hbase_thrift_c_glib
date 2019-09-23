4b2b8a1c95e67580a299bcbd8cbeaa774696d412
oun111 <oun111@github.com>
2019-09-23 09:43:14 +0800

    add new source files

---
+++ Makefile
@@ -0,0 +1,37 @@
+
+GEN_DIR:= $(shell pwd)/.gen/
+
+C_SRCS         := $(notdir $(shell find . -maxdepth 1 -name "*.c"))
+C_OBJS         := $(patsubst %.c,$(GEN_DIR)/%.o,$(C_SRCS))
+OBJS           := $(C_OBJS)
+
+
+THRIFT_BASE    := /home/user1/work/thrift-0.12.0/install
+
+LDFLAG         := -L$(THRIFT_BASE)/lib -lthrift -lthrift_c_glib -lglib-2.0 -lgobject-2.0 
+CFLAG_OBJS     := -Wall -Werror -g -I. -I$(THRIFT_BASE)/include -I/usr/include/glib-2.0/
+TARGET_EXEC    := th_app
+ 
+
+.PHONY: all
+all: mkdir  $(TARGET_EXEC) 
+
+mkdir:
+	$(shell mkdir  $(GEN_DIR))
+
+$(TARGET_EXEC): $(OBJS)
+	$(CC) $(OBJS) -o $@  $(LDFLAG)
+
+
+%.o:../%.c
+	$(CC) $(CFLAG_OBJS) -c $< -o $@
+
+.PHONY: clean
+clean: 
+	rm -rf $(OBJS) $(TARGET_EXEC) $(GEN_DIR)
+
+.PHONY: distclean
+distclean:clean
+	rm -rf cscope.* 
+	rm -rf tags
+
---
+++ main.c
@@ -0,0 +1,145 @@
+#include "t_h_base_service.h"
+#include <thrift/c_glib/thrift.h>
+#include <thrift/c_glib/thrift_application_exception.h>
+#include <thrift/c_glib/protocol/thrift_binary_protocol.h>
+#include <thrift/c_glib/protocol/thrift_compact_protocol.h>
+#include <thrift/c_glib/protocol/thrift_multiplexed_protocol.h>
+#include <thrift/c_glib/transport/thrift_buffered_transport.h>
+#include <thrift/c_glib/transport/thrift_framed_transport.h>
+#include <thrift/c_glib/transport/thrift_ssl_socket.h>
+#include <thrift/c_glib/transport/thrift_socket.h>
+#include <thrift/c_glib/transport/thrift_transport.h>
+
+
+int test_exists(THBaseServiceIf *client, const gchar *tablename)
+{
+  GByteArray *table = NULL ;
+  TGet *tget = NULL ;
+  gboolean ret = FALSE;
+  TIOError *io = NULL;
+  GError *error = NULL;
+
+
+  printf("testing 'exists' command for talbe '%s'...\n",tablename);
+
+  tget = g_object_new(TYPE_T_GET,NULL);
+
+  tget->row = g_byte_array_new();
+  g_byte_array_append(tget->row,(guint8*)"0",strlen("0"));
+
+  table= g_byte_array_new();
+  g_byte_array_append(table,(guint8*)tablename,strlen(tablename));
+
+
+  ret = t_h_base_service_client_exists(client,&ret,table,tget,&io,&error);  
+  if (ret==FALSE) {
+    printf("client exists fail(%d): %s (%s)\n",
+           error->code,error->message,io->message);
+  }
+  else {
+    printf("success!\n");
+  }
+
+  g_byte_array_unref(table);
+  g_object_unref(tget);
+  g_object_unref(io);
+
+  return 0;
+}
+
+int test_get(THBaseServiceIf *client, const gchar *tablename)
+{
+  GByteArray *table = NULL ;
+  TGet *tget = NULL ;
+  gboolean ret = FALSE;
+  TIOError *io = NULL;
+  GError *error = NULL;
+  TResult *result= NULL;
+  gchar *rno = "no-001001";
+
+
+  printf("testing 'get' command for talbe '%s'...\n",tablename);
+
+  // construct TGet
+  tget = g_object_new(TYPE_T_GET,NULL);
+  tget->row = g_byte_array_new();
+  g_byte_array_append(tget->row,(guint8*)rno,strlen(rno));
+
+  // construct table
+  table= g_byte_array_new();
+  g_byte_array_append(table,(guint8*)tablename,strlen(tablename));
+
+
+  ret = t_h_base_service_client_get(client,&result,table,tget,&io,&error);  
+  if (ret==FALSE) {
+    printf("client get fail(%d): %s (%s)\n",error?error->code:-1,
+           error?error->message:"n/A",io?io->message:"N/a");
+  }
+  else {
+    printf("success!\n");
+  }
+
+  g_byte_array_unref(table);
+  g_object_unref(tget);
+  g_object_unref(result);
+  g_object_unref(io);
+
+  return 0;
+}
+
+int test_t_h_base_service_client()
+{
+  ThriftSocket *socket = NULL;
+  ThriftProtocol *protocol= NULL;
+  THBaseServiceIf *client = NULL;
+  ThriftTransport *transport = NULL;
+  gchar *host = "127.0.0.1";
+  gint port = 9090;
+  
+
+  socket = g_object_new(THRIFT_TYPE_SOCKET,"hostname",host,
+                        "port",port,NULL);
+
+
+  transport = g_object_new(THRIFT_TYPE_BUFFERED_TRANSPORT,
+                           "transport",socket,NULL);
+
+  thrift_transport_open(transport, NULL);
+  if (!thrift_transport_is_open(transport)) {
+    printf("open socket fail!\n");
+    goto __done;
+  }
+
+  protocol = g_object_new(THRIFT_TYPE_BINARY_PROTOCOL,
+                          "transport",transport,NULL);
+
+  client = g_object_new(TYPE_T_H_BASE_SERVICE_CLIENT,
+                        "input_protocol",protocol,
+                        "output_protocol",protocol,
+                        NULL);
+
+  /*
+   * case 1: 
+   */
+  test_exists(client,"user2");
+
+  /*
+   * case 2: 
+   */
+  test_get(client,"user1");
+
+__done:
+  g_object_unref(client);
+  g_object_unref(protocol);
+  g_object_unref(transport);
+  g_object_unref(socket);
+
+  return 0;
+}
+
+int main()
+{
+  test_t_h_base_service_client();
+  return 0;
+}
+
