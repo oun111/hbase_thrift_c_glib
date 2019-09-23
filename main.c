f5bc6fe81259f1d0e8451d02fa44deaaff849dad
oun111 <oun111@github.com>
2019-09-23 17:54:31 +0800

    fixup 'tget' logics

--- main.c
+++ main.c
@@ -69,6 +69,8 @@ int test_get(THBaseServiceIf *client, const gchar *tablename)
   table= g_byte_array_new();
   g_byte_array_append(table,(guint8*)tablename,strlen(tablename));
 
+  // result
+  result = g_object_new(TYPE_T_RESULT,NULL);
 
   ret = t_h_base_service_client_get(client,&result,table,tget,&io,&error);  
   if (ret==FALSE) {
@@ -76,12 +78,62 @@ int test_get(THBaseServiceIf *client, const gchar *tablename)
            error?error->message:"n/A",io?io->message:"N/a");
   }
   else {
+    printf("row: %s\n",result->row->data);
+
+    for (int i=0;i<result->columnValues->len;i++) {
+      TColumnValue *pcol = result->columnValues->pdata[i];
+      printf("col family '%s', value '%s'\n",(char*)pcol->family->data,(char*)pcol->value->data);
+    }
+
     printf("success!\n");
   }
 
   g_byte_array_unref(table);
   g_object_unref(tget);
   g_object_unref(result);
+  g_object_unref(io);
+
+  return 0;
+}
+
+int test_put(THBaseServiceIf *client, const gchar *tablename, 
+             const gchar *cf, const gchar *val)
+{
+  GByteArray *table = NULL ;
+  TPut *tput = NULL ;
+  gboolean ret = FALSE;
+  TIOError *io = NULL;
+  GError *error = NULL;
+  gchar *rno = "no-001001";
+
+
+  printf("testing 'put' command for talbe '%s'...\n",tablename);
+
+  // construct TPut
+  tput = g_object_new(TYPE_T_PUT,NULL);
+  tput->row = g_byte_array_new();
+  g_byte_array_append(tput->row,(guint8*)rno,strlen(rno));
+#if 0
+  tput->columnValues = g_ptr_array_new();
+  g_ptr_array_add(tput->columnValues,(gpointer)cf);
+  g_ptr_array_add(tput->columnValues,(gpointer)val);
+#endif
+
+  // construct table
+  table= g_byte_array_new();
+  g_byte_array_append(table,(guint8*)tablename,strlen(tablename));
+
+  ret = t_h_base_service_client_put(client,table,tput,&io,&error);  
+  if (ret==FALSE) {
+    printf("client put fail(%d): %s (%s)\n",error?error->code:-1,
+           error?error->message:"n/A",io?io->message:"N/a");
+  }
+  else {
+    printf("success!\n");
+  }
+
+  g_byte_array_unref(table);
+  g_object_unref(tput);
   g_object_unref(io);
 
   return 0;
@@ -126,7 +178,12 @@ int test_t_h_base_service_client()
   /*
    * case 2: 
    */
-  test_get(client,"user1");
+  //test_get(client,"user1");
+
+  /*
+   * case 3: 
+   */
+  test_put(client,"user1","name","JOey");
 
 __done:
   g_object_unref(client);
